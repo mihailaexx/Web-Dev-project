9e8f653b0993b7a56a16884b0bae1c529ced1010
mihailaexx <m_bulushev@kbtu.kz>
2025-04-04 00:42:15 +0500

    feat: add authentication interceptor and update routing for user profiles and authentication pages, complete header

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,26 +1,46 @@
 import {Routes} from '@angular/router';
-import {HomeComponent} from './home/home.component';
 import {ShopComponent} from './shop/shop.component';
 import {ProductdetailComponent} from './productdetail/productdetail.component';
+import {ProfileComponent} from './profile/profile.component';
+import {CartComponent} from './cart/cart.component';
+import {LikeComponent} from './like/like.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
-    title: 'Final project site',
-  },
-  {
-    path: 'shop',
     component: ShopComponent,
-    title: 'Shop',
+    title: 'Final project site',
   },
   {
     path: 'shop/p/:slug',
     component: ProductdetailComponent,
     title: 'Buy :slug',
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    title: 'Profile Page',
+  },
+  {
+    path: 'profile/basket',
+    component: CartComponent,
+    title: 'Basket',
+  },
+  {
+    path: 'profile/likes',
+    component: LikeComponent,
+    title: 'Likes',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login Page',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register Page',
   }
-  // {
-  //   path: '',
-  //   redirectTo: '',
-  // },
 ];
