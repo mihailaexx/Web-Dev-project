ba7a2c66d29ddc37aae94b29a8cddfe5022bdd98
mihailaexx <m_bulushev@kbtu.kz>
2025-04-04 01:46:45 +0500

    change header tag in header.component.css and invent wheel :D (create photo carousel with stop on hover)

--- front/src/app/shop/shop.component.ts
+++ front/src/app/shop/shop.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-shop',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
-export class ShopComponent {
 
+export class ShopComponent implements OnInit, OnDestroy {
+  currentImageIndex = 0
+  private intervalId: any
+  images: string[] = [
+    'IMG_6435-1.jpg',
+    'IMG_7960.jpg',
+    'IMG_8074.jpg',
+    'IMG_8198.jpg',
+    'IMG_8202.jpg',
+  ];
+
+  ngOnInit() {
+    this.startSlideshow()
+  }
+
+  ngOnDestroy() {
+    this.stopSlideshow()
+  }
+
+  nextSlide() {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+  }
+
+  startSlideshow() {
+    this.intervalId = setInterval(() => {
+      this.nextSlide();
+    }, 3000);
+  }
+
+  stopSlideshow() {
+    clearInterval(this.intervalId);
+  }
 }
